9640ff1d1e723038104f12ca512630d19f190b36
abhisheksharma <sharma.abhi3199@gmail.com>
2025-02-26 00:00:04 +0530

    new commit with pub template

--- demo/router/admins.js
+++ demo/router/admins.js
@@ -2,21 +2,15 @@ const express = require('express')
 
 const fs = require('fs')
 const path = require('path')
+const rootDir = require('../utils/path')
 
 const adminRouter = express.Router();
 
-adminRouter.post('/add',(req,res,next)=>{
-    console.log("Adding name in express")
-    console.log(req.body)
-    fs.writeFile('name.txt',req.body.name,(err)=>{
-                    if(err) console.log("give the erro",err)
-                    res.redirect('/')
-                })
-})
+
 
 adminRouter.get('/name',(req,res,next)=>{
     console.log("i am in name")
-    res.sendFile(path.join(__dirname,'..','views','form-user.html'))
+    res.sendFile(path.join(rootDir,'views','form-user.html'))
 })
 
 module.exports = {
--- demo/server.js
+++ demo/server.js
@@ -7,37 +7,52 @@
 
 
 const express = require('express')
-
 const adminRouter = require('./router/admins.js')
+const path = require('path')
+const rootDir = require('./utils/path.js')
+const fs = require('fs')
 
 // require is common js method
 
 // import is es modules
 const app = express()
 const port = 3000
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
-app.use((req,res,next)=>{
-    console.log("i am express server")
-    // res.send('Hello World')
-    next()
-})
-app.use((req,res,next)=>{
-    console.log("i am server")
-    // res.send('Hello World')
-    next()
-})
+
+app.set('view engine', 'pug')
+app.set('views','views')
+// app.use((req,res,next)=>{
+//     console.log("i am express server")
+//     // res.send('Hello World')
+//     next()
+// })
+// app.use((req,res,next)=>{
+//     console.log("i am server")
+//     // res.send('Hello World')
+//     next()
+// })
+const users = []; // in memory.
 
 app.use('/admin',adminRouter.router)
 
+app.post('/admin/add',(req,res)=>{
+    console.log("Adding name in express")
+    console.log(req.body)
+    users.push(req.body.name);
+    console.log(users)
 
-// console.log(adminRouter.name)
-
-
-app.get('/',(req,res)=>{
-    res.send(" Hello Welcome to your Node js Page")
+    fs.writeFile('name.txt',req.body.name,(err)=>{
+                    if(err) console.log("give the erro",err)
+                    res.redirect('/')
+    })
 })
+app.get('/',(req,res)=>{
+    res.render('home',{path:'/',page:'Home page',users:users})
+})
+
 app.get('*',(req,res)=>{
     res.send("Error can't find any page")
 })
@@ -50,7 +65,6 @@ app.post('*',(req,res)=>{
 app.listen(port,()=>{{
     console.log("server is runn,",port)
 }})
-
 
 
 
