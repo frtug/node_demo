31a42ef5380580b20a897e9ee29818116409eb1c
abhisheksharma <sharma.abhi3199@gmail.com>
2025-02-28 21:38:29 +0530

    updated with models filesystem read nd write

--- demo/controllers/album.js
+++ demo/controllers/album.js
@@ -1,6 +1,13 @@
 const Movie = require('../models/movies')
 const fs = require('fs')
 
+module.exports.getDetail = (req,res)=>{
+    console.log("i am in name")
+    res.json({"details":req.params.id})
+    // TODO to add details page with single card 
+    res.render('cardDetail',{})
+}
+
 module.exports.addMovie = (req,res,next)=>{
     console.log("i am in name")
     // res.sendFile(path.join(rootDir,'views','form-user.html'))
@@ -9,7 +16,7 @@ module.exports.addMovie = (req,res,next)=>{
 module.exports.addingMovie = (req,res)=>{
     console.log("Adding name in express")
     // users.push(req.body.name);
-    const movie1 = new Movie(req.body.name)
+    const movie1 = new Movie(req.body.name,"A daring crew embarks on an interstellar journey","Sci-Fi","4")
     movie1.save();
     res.redirect('/')
 
