6b502fdc92788c6d821a2f588218535709357aa0
abhisheksharma <sharma.abhi3199@gmail.com>
2025-03-27 19:42:57 +0530

    updated with JWT token

--- quiz-api/index.js
+++ quiz-api/index.js
@@ -1,22 +1,52 @@
 const express = require('express');
 const cors = require('cors');
+
 const fs = require('fs');
 const path = require('path');
 const jwt = require('jsonwebtoken')
 const app = express();
 const PORT = 5000;
 
+
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 // Load questions from JSON file
 const questionsPath = path.join(__dirname, 'data', 'questions.json');
 const questions = JSON.parse(fs.readFileSync(questionsPath, 'utf-8'));
 
+// Load questions from JSON file
+
 const ansPath = path.join(__dirname, 'data', 'answers.json');
 const answers = JSON.parse(fs.readFileSync(ansPath, 'utf-8'));
 
+let token = ""
+
+function verifyToken(req,res,next){
+  const auth = req.headers['authorization'];
+  console.log(auth)
+  const token = auth && auth.split(" ")[1];
+  
+  console.log(token)
+  let isError = false;
+  try{
+    jwt.verify(token,"My Key",(err,decoded)=>{
+      if(err){
+        throw err('Failed verification')
+      }
+      console.log("Value is correct",decoded)
+    })
+  }
+  catch(err){
+    return res.status(400).json({"msg":"Verification Failed"})
+  }
+  next()
+}
 // API endpoint to get a specific question
 app.get('/api',(req,res)=>{
     const totalQuestion = questions.length;
@@ -24,6 +54,8 @@ app.get('/api',(req,res)=>{
     res.status(200).json({totalQuestion:totalQuestion})
 })
 app.get('/api/questions/:id', (req, res) => {
+  
+
   const questionId = parseInt(req.params.id);
   const question = questions.find(q => q.id === questionId);
   
@@ -35,7 +67,7 @@ app.get('/api/questions/:id', (req, res) => {
 });
 
 // API endpoint to verify answer
-app.post('/api/verify-answer', (req, res) => {
+app.post('/api/verify-answer',verifyToken, (req, res) => {
   const { questionId, selectedOption } = req.body;
   console.log(questionId,selectedOption)
   const q = questions.find(q => q.id === questionId);
@@ -52,16 +84,19 @@ app.post('/api/verify-answer', (req, res) => {
 app.post('/api/auth/login',(req,res)=>{
     const {email,password} = req.body;
     console.log(req.body)
-    res.status(200).json({message:"success"})
+    
+    res.status(200).json({message:"success",token:token})
 })
 app.post('/api/auth/signup',(req,res)=>{
     const {name,email,password,confirmPassword} = req.body;
     console.log(req.body)
-    const token = jwt.sign({
+    const access_token = jwt.sign({
         email:email
     },"My Key")
+    token = access_token
     res.status(200).json({message:"success",token:token})
 })
+
 
 // Start server
 app.listen(PORT, () => {
